21c936d055c591160f7921ff882d722e8f20a79f
Michael Westblade <michaelwestblade@gmail.com>
2023-07-29 12:21:21 -0500

    basic sql

--- section2/filter-data-with-where.sql
+++ section2/filter-data-with-where.sql
@@ -7,4 +7,6 @@ select * from employees where department = 'Clothing' and salary > 90000;
 
 select * from employees where department = 'Clothing' OR salary > 90000;
 
+select * from employees where salary < 40000 and (department = 'Clothing' or department = 'Pharmacy');
+
 select * from employees where salary < 40000 and department in ('Clothing', 'Pharmacy');
